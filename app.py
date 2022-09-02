1aa18d039d5ab41aff9ec3bfeca1ef8f266972a4
Keiji Shinoda <k.shinoda.2000@hotmail.com>
2022-09-02 00:59:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ def response():
     query = dict(request.form)['query']
     if 'medico' in query or 'Medico' in query or 'Médico' in query or 'médico' in query:
         result = "La lista de médicos es: medico 1, medico 2, medico 3, etc..."
-    elif 'solicitud' in query:
+    elif 'solicitud' in query or 'Solicitud' in query:
         result = "Para realizar una solicitud debe completar los siguientes datos:"
     else:
-        result = query + " " + time.ctime()
+        result = "Aun me estoy entrenando para ayudarte con eso."
+        
     return jsonify({"response" : result})
 
 if __name__ == "__main__":
